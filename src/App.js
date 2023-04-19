61f692fd5d59208fb03c1c4ebbb9ce08711ccf40
ShogherAndreasyan <shogher9977@gmail.com>
2023-04-19 23:17:07 +0400

    add button - which will change a price from AMD to USD, and the opposite

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import GetName from './Name';
-import GetAge from './Age';
-import Profession from './Profession';
+// import GetName from './Name';
+// import GetAge from './Age';
+// import Profession from './Profession';
 import Name from './Name61';
 import Price from './Price61';
 import Description from './Description61';
@@ -13,36 +13,73 @@ class Product extends Component{
   render(){
     return(
       <div>
-        <Name name = {this.props.name}/> <Price price = {this.props.price}/> <Description description = {this.props.description}/>
+        <Name name = {this.props.name}/> 
+        <Price price = {this.props.price}/> 
+        <Description description = {this.props.description}/>
       </div>
     )
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <p><GetName name ="Annie"/> is <GetAge age = {27}/> years old. She is a <Profession prof = "developer"/>.</p> */}
-        {/*---lesson 61---*/}
-        <div>
-          <Product name="banabas" price="1$" description="Fresh bananas from Ecuador"/>
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    amd: 0,
+    usdRate: 391,
+
+    products: [
+      {
+        name: 'banana',
+        price: '3',
+        description: 'fruit1'
+      },
+      {
+        name: 'apple',
+        price: '2',
+        description: 'fruit2'
+      },
+      {
+        name: 'orange',
+        price: '4',
+        description: 'fruit3'
+      },
+    ]
+  }
+
+  inputChange = (event) =>{
+    console.log(event.target.value);
+    this.setState({
+      amd: event.target.value,
+    });
+  };
+
+  render(){
+    const productComponents = this.state.products.map(el => {
+      return(
+        <Product
+        key = {el.name}
+        name = {el.name}
+        price = {el.price}
+        description = {el.description}
+        />
+      )
+    });
+  
+    const usd = (this.state.amd / this.state.usdRate).toFixed(2)
+    return (
+      <div className="App">
+          <div>
+            {productComponents}
+          </div>
+  
+          AMD: <input
+            type='number'
+            placeholder='AMD'
+            onChange={this.inputChange}
+          />
+          <input type='text' value={usd} readOnly={true}/> :USD
+      </div>
+    );
+  }
 }
 
 export default App;
